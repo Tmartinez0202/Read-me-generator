a0703b36ecbbf0b76a0b5dead0d75bab5d87c4ad
OkieKenobi <travismartinez88@gmail.comgit config --global user.name Tmartinez0202>
2020-07-19 22:41:06 -0600

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,48 @@
 // array of questions for user
+const inquirer = require("inquirer");
+
+
+
+
 const questions = [
+    inquirer.prompt([
+        {
+            type: "input",
+            message:"What is your github name",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is your email",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your project name",
+            name: "project"
+        },
+        {
+            type: "input",
+            message: "Please write a short description of your project",
+            name: "description"
+        },
+        {
+            type:"list",
+            message:"What kind of license should your project have",
+            name: "license",
+            choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"]
+        },
+        {
+            type: "input",
+            message: "what command should be run to install dependencies",
+            default: "npm i",
+            name: "install"
+        },
+        {
+            
+        }
+
+    ])
 
 ];
 
